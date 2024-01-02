5758580483141ff2772a5129a4af1e828f61c336
Prahalad Mankoliya <prahaladj12345@gmail.com>
2024-01-02 09:06:15 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignUpForm from './Components/SignUpLogIn/SignUpForm'
+
+const App = () => {
+  return (
+    <>
+    <SignUpForm/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/SignUpLogIn/SignUpForm.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import { Button, Form } from "react-bootstrap";
+import classes from "./SignUpForm.module.css";
+
+
+const SignUpForm = () => {
+    return (
+        <>
+            {forgetVisisble ? <ForgotPassForm onReset={() => setForgetVisible(false)} onBack={onBack} /> : (<div className={classes.signup}>
+                <h1>{!isLogin ? "SignUp" : "LogIn"}</h1>
+                <Form ref={formRef} onSubmit={submitHandler}>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control
+                            type="email"
+                            placeholder="Enter email"
+                            ref={emailInputRef}
+                            required
+                        />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control
+                            type="password"
+                            placeholder="Password"
+                            ref={passInputRef}
+                            required
+                        />
+                    </Form.Group>
+                    {!isLogin && <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Confirm Password</Form.Label>
+                        <Form.Control
+                            type="password"
+                            placeholder="Confirm Password"
+                            ref={conPassInputRef}
+                            required
+                        />
+                    </Form.Group>}
+                    {isLogin && <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Link onClick={linkClickHandler}>Forgot Password?</Link>
+                    </Form.Group>}
+                    {!isLoading ? <Button variant="primary" type="submit">
+                        {isLogin ? "LogIn" : "SignUp"}
+                    </Button> : <Button variant="primary">Sending Request...</Button>}
+                    {varifyMail && (
+                        <p style={{ margin: "1rem", color: "green" }}>
+                            Please varify email. Verfication mail already sent.
+                        </p>
+                    )}
+                </Form>
+                {!isLogin && <span>Already have an account?<button
+                    className={classes.toggle}
+                    onClick={switchAuthModeHandler}>LogIn</button></span>}
+                {isLogin && <span>Don't have an account?<button
+                    className={classes.toggle}
+                    onClick={switchAuthModeHandler}>SignUp</button></span>}
+            </div>)}
+
+
+
+
+        </>
+    )
+}
+
+export default SignUpForm
